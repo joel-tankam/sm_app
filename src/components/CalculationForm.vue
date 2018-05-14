07cf72678f5c1f1fee90b87b50e431087fb10e20
Joël Tankam <joel.tankam@gmail.com>
2018-05-14 21:01:03 +0000

    Fix mixed options interaction

--- src/components/CalculationForm.vue
+++ src/components/CalculationForm.vue
@@ -3,15 +3,15 @@
     <b-form @submit.prevent="onSubmit" @reset.prevent="onReset">
       <b-form-group horizontal
         breakpoint="md"
-        label="Besoin en safi" label-size="md" label-class="font-weight-bold">
+        label="Besoin de safi" label-size="md" label-class="font-weight-bold">
         <b-form-group horizontal
-          label="Tonnage:" label-class="text-sm-right" label-for="neededTonnage">
+          label="Tonnage :" label-class="text-sm-right" label-for="neededTonnage">
           <b-form-input required id="neededTonnage"
             type="number" step="10"
             v-model="form.needed.tonnage"/>
         </b-form-group>
         <b-form-group horizontal
-          label="Teneur:" label-class="text-sm-right" label-for="neededContent"
+          label="Teneur :" label-class="text-sm-right" label-for="neededContent"
           description="Pourcentage (%)">
           <b-form-input required id="neededContent"
             type="number" step="0.01" min="50" max="60"
@@ -21,28 +21,34 @@
       <b-form-group horizontal breakpoint="md"
         label="Couche de départ" label-size="md" label-class="font-weight-bold">
         <b-form-group horizontal
-          label="Couche:" label-class="text-sm-right" label-for="givenLayer">
-          <b-form-select required
-            v-model="form.given.Layer" :options="LayersOptions" />
+          label="Couche :" label-class="text-sm-right" label-for="givenLayer">
+          <b-form-select required id="givenLayer"
+            v-model="form.given.layer" :options="layersOptions" />
         </b-form-group>
         <b-form-group horizontal
-          label="Tonnage:" label-class="text-sm-right" label-for="givenTonnage">
+          label="Tonnage :" label-class="text-sm-right" label-for="givenTonnage">
           <b-form-input required id="givenTonnage"
             type="number"
             v-model="form.given.tonnage"/>
         </b-form-group>
+      </b-form-group>
+      <b-form-group horizontal breakpoint="md"
+        label="Couches à mélanger" label-size="md" label-class="font-weight-bold">
         <b-form-group horizontal
-          label="Teneur:" label-class="text-sm-right" label-for="givenContent"
-          description="Pourcentage (%)">
-          <b-form-input required id="givenContent"
-          type="number" step="0.01" min="50" max="60"
-          v-model="form.given.content"/>
+          label="Choix :"  label-class="text-sm-right">
+          <b-form-checkbox-group
+            v-model="form.given.mixedLayers">
+            <b-form-checkbox v-for="i in mixedLayersOptions" v-bind:key="i.value"
+              :value="i.value" :disabled="i.value === form.given.layer">
+                {{ i.text }}
+              </b-form-checkbox>
+          </b-form-checkbox-group>
         </b-form-group>
-        <b-form-group horizontal
-          label="Granulométrie:" label-class="text-sm-right" label-for="givenGranulometry">
-          <b-form-input required id="givenGranulometry"
-            type="number" step="0.01"
-            v-model="form.given.granulometry"/>
+        <b-form-group horizontal v-show="additionalLayersNumber > 0"
+          label="Tonnages fixés :" label-class="text-sm-right">
+          <b-form-input v-for="i in additionalLayersNumber" v-bind:key="i" required
+            name="additionalTonnages" type="number" min="1" class="my-2"
+            v-model="form.given.additionalTonnages[i-1]" @change="checkTonnages(i-1)" />
         </b-form-group>
       </b-form-group>
       <b-button type="submit" variant="primary">Calculer</b-button>
@@ -52,7 +58,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Watch, Vue } from 'vue-property-decorator';
 import { Layer } from '@/models/layer';
 import { ObjectUtil } from '@/util/object';
 
@@ -67,11 +73,23 @@ export default class CalculationForm extends Vue {
     given: {
       layer: Layer.Couche0,
       tonnage: 5000,
+      mixedLayers: [Layer.Couche1, Layer.Couche2],
+      additionalTonnages: [0, 0, 0, 0, 0],
     },
   };
 
   // Datas
-  private LayersOptions: any[] = [
+  private layersOptions: any[] = [
+    { value: Layer.Couche0, text: Layer.Couche0 },
+    { value: Layer.Couche1, text: Layer.Couche1 },
+    { value: Layer.Couche2, text: Layer.Couche2 },
+    { value: Layer.Couche3, text: Layer.Couche3 },
+    { value: Layer.Couche4, text: Layer.Couche4 },
+    { value: Layer.Couche5, text: Layer.Couche5 },
+    { value: Layer.SillonA, text: Layer.SillonA },
+    { value: Layer.SillonB, text: Layer.SillonB },
+  ];
+  private mixedLayersOptions: any[] = [
     { value: Layer.Couche0, text: Layer.Couche0 },
     { value: Layer.Couche1, text: Layer.Couche1 },
     { value: Layer.Couche2, text: Layer.Couche2 },
@@ -84,11 +102,39 @@ export default class CalculationForm extends Vue {
   private form: any = ObjectUtil.deepCopy(CalculationForm.FORM_INITIAL_VALUES);
 
   // Methods
+  private checkTonnages(i: any) {
+    const sumFunction = (acc: number, current: number) =>
+      Number(acc) + Number(current);
+    let sum = this.form.given.additionalTonnages.reduce(sumFunction);
+    const tonnage = Number(this.form.needed.tonnage);
+    if (sum >= tonnage) {
+      sum -= Number(this.form.given.additionalTonnages[i]);
+      this.form.given.additionalTonnages[i] = Number(tonnage) - sum;
+    }
+    this.$forceUpdate();
+  }
   private onSubmit(evt: any) {
-    console.log(this.form);
+    this.$emit('formUpdated', this.form);
   }
   private onReset(evt: any) {
     this.form = ObjectUtil.deepCopy(CalculationForm.FORM_INITIAL_VALUES);
+  }
+
+  // Computed properties
+  get additionalLayersNumber() {
+    return this.form.given.mixedLayers.length - 2;
+  }
+
+  // Watch
+  @Watch("additionalLayersNumber")
+  onAdditionalLayersNumberChanged(val: number, oldVal: number) {
+    if (oldVal > val) {
+      console.log(val);
+      for (let i = val; i <= oldVal; i += 1) {
+        this.form.given.additionalTonnages[i] = 0;
+      }
+      this.$forceUpdate();
+    }
   }
 }
 </script>
